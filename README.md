cae561e196204d4cbd68d5bc918bda5a6d1df81b
José Fernando Falcao <jfribeirofalcao@gmail.com>
2017-11-19 21:54:26 -0500

    Lesson 11 completed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # nbwebsite
 
-TODO: Bookmark Video https://www.youtube.com/watch?v=dONYOtb2ySI&list=PL6gx4Cwl9DGBlmzzFcLgDhKTTfNLfX1IK&index=11
+TODO: Bookmark Video https://www.youtube.com/watch?v=dONYOtb2ySI&list=PL6gx4Cwl9DGBlmzzFcLgDhKTTfNLfX1IK&index=13
--- music/views.py
+++ music/views.py
@@ -4,3 +4,6 @@ from django.http import  HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<H1>This is the Music App Homepage</h1>')
+
+def detail(response, album_id):
+    return HttpResponse('<H2>Details for AlbumID: ' + str(album_id) + '</H2>')
